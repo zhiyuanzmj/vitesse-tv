51b8301c2f47ac595529d9b2440f1443a4601103
hwq <13172331570@163.com>
2023-07-07 18:54:16 +0800

    update

--- src/views/service/index.vue
+++ src/views/service/index.vue
@@ -16,28 +16,62 @@ const list = [
       <!-- <button mr-5 rounded-lg style="background-image: url('/images/service.png')" bg-cover bg-no-repeat /> -->
       <!-- <button mr-5 relative rounded-lg grow="1.5" style="background-image: url('/images/service-1.png')" bg-cover bg-no-repeat>
       </button> -->
-      <div grow-2 flex-1 grow-3 relative>
-        <video preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover poster="/images/service.png" @click="$event.target.webkitRequestFullscreen()">
+      <!-- style="background-color: #9ca3af;" -->
+      <RouterLink :to="{ path: `/service/service` }" flex="~ col" grow-1 rounded-lg style="background-color: #EF7D3E;"
+        items-center justify-center>
+        <img src="/images/service.svg" mt-auto mb-3>
+        <span text-lg mb-auto z-1>居家守护</span>
+      </RouterLink>
+
+      <div grow-2 flex-1 grow-1 mx-5 relative>
+        <video preload="none" w-full h-full absolute rounded-lg bg-cover controls object-cover
+          poster="/images/service-1.png" @click="$event.target.webkitRequestFullscreen()">
           <source src="/images/test.mp4" type="video/mp4">
         </video>
       </div>
 
-      <div grow-2 flex-1 grow-1 mx-5 relative>
-        <video preload="none" w-full h-full absolute rounded-lg bg-cover controls object-cover poster="/images/service-1.png" @click="$event.target.webkitRequestFullscreen()">
+      <div grow-2 flex-1 grow-3 relative>
+        <video preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
+          poster="/images/service.png" @click="$event.target.webkitRequestFullscreen()">
           <source src="/images/test.mp4" type="video/mp4">
         </video>
       </div>
-      <RouterLink :to="{ path: `/service/service` }" flex="~ col" grow-1 style="background-color: #9ca3af;" rounded-lg items-center justify-center>
-        <img src="/images/service.svg" mt-auto mb-3>
-        <span text-lg mb-auto>居家守护</span>
-      </RouterLink>
     </div>
 
     <div flex="~ 1">
-      <RouterLink v-for="(i, index) in list" :key="i.label" :to="{ path: `/service/service${index}` }" mr-5 :style="i.style" rounded-lg flex="~ col 1" items-center justify-center>
+      <div flex-1 grow-1 mr-5 relative>
+        <video preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
+          poster="/images/service1.png" @click="$event.target.webkitRequestFullscreen()">
+          <source src="/images/test.mp4" type="video/mp4">
+        </video>
+      </div>
+
+      <div flex-1 grow-1 mr-5 relative>
+        <video preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
+          poster="/images/service2.png" @click="$event.target.webkitRequestFullscreen()">
+          <source src="/images/test.mp4" type="video/mp4">
+        </video>
+      </div>
+
+      <div flex-1 grow-1 mr-5 relative>
+        <video preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
+          poster="/images/service3.png" @click="$event.target.webkitRequestFullscreen()">
+          <source src="/images/test.mp4" type="video/mp4">
+        </video>
+      </div>
+
+      <div flex-1 grow-1 relative>
+        <video preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
+          poster="/images/service4.png" @click="$event.target.webkitRequestFullscreen()">
+          <source src="/images/test.mp4" type="video/mp4">
+        </video>
+      </div>
+
+      <!-- <RouterLink v-for="(i, index) in list" :key="i.label" :to="{ path: `/service/service${index}` }" mr-5
+        :style="i.style" rounded-lg flex="~ col 1" items-center justify-center>
         <img :src="`images/service${index}.svg`" mt-auto mb-3>
         <span text-lg mb-auto>{{ i.label }}</span>
-      </RouterLink>
+      </RouterLink> -->
     </div>
   </div>
 </template>
