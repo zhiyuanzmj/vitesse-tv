71d3c850af8e44cd552aeeac8c89af554efe0cb6
gaokefei <260480378@qq.com>
2023-10-27 10:44:14 +0800

    feat: completed index

--- src/views/service/index.vue
+++ src/views/service/index.vue
@@ -17,22 +17,28 @@ const list = [
       <!-- <button mr-5 relative rounded-lg grow="1.5" style="background-image: url('/images/service-1.png')" bg-cover bg-no-repeat>
       </button> -->
       <!-- style="background-color: #9ca3af;" -->
-      <RouterLink :to="{ path: `/service/service` }" flex="~ col" grow-1 rounded-lg style="background-color: #EF7D3E;"
-        items-center justify-center>
+      <RouterLink
+        :to="{ path: `/service/service` }" flex="~ col" grow-1 rounded-lg style="background-color: #EF7D3E;"
+        items-center justify-center
+      >
         <img src="/images/service.svg" mt-auto mb-3>
         <span text-lg mb-auto z-1>居家守护</span>
       </RouterLink>
 
       <div grow-2 flex-1 grow-1 mx-5 relative>
-        <video preload="none" w-full h-full absolute rounded-lg bg-cover controls object-cover
-          poster="/images/service-1.png" @click="$event.target.webkitRequestFullscreen()">
+        <video
+          preload="none" w-full h-full absolute rounded-lg bg-cover controls object-cover
+          poster="/images/service-1.png" @click="$event.target.webkitRequestFullscreen()"
+        >
           <source src="/images/test.mp4" type="video/mp4">
         </video>
       </div>
 
       <div grow-2 flex-1 grow-3 relative>
-        <video preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
-          poster="/images/service.png" @click="$event.target.webkitRequestFullscreen()">
+        <video
+          preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
+          poster="/images/service.png" @click="$event.target.webkitRequestFullscreen()"
+        >
           <source src="/images/test.mp4" type="video/mp4">
         </video>
       </div>
@@ -40,29 +46,37 @@ const list = [
 
     <div flex="~ 1">
       <div flex-1 grow-1 mr-5 relative>
-        <video preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
-          poster="/images/service1.png" @click="$event.target.webkitRequestFullscreen()">
+        <video
+          preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
+          poster="/images/service1.png" @click="$event.target.webkitRequestFullscreen()"
+        >
           <source src="/images/test.mp4" type="video/mp4">
         </video>
       </div>
 
       <div flex-1 grow-1 mr-5 relative>
-        <video preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
-          poster="/images/service2.png" @click="$event.target.webkitRequestFullscreen()">
+        <video
+          preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
+          poster="/images/service2.png" @click="$event.target.webkitRequestFullscreen()"
+        >
           <source src="/images/test.mp4" type="video/mp4">
         </video>
       </div>
 
       <div flex-1 grow-1 mr-5 relative>
-        <video preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
-          poster="/images/service3.png" @click="$event.target.webkitRequestFullscreen()">
+        <video
+          preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
+          poster="/images/service3.png" @click="$event.target.webkitRequestFullscreen()"
+        >
           <source src="/images/test.mp4" type="video/mp4">
         </video>
       </div>
 
       <div flex-1 grow-1 relative>
-        <video preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
-          poster="/images/service4.png" @click="$event.target.webkitRequestFullscreen()">
+        <video
+          preload="none" webkit-playsinline="true" w-full h-full absolute rounded-lg bg-cover controls object-cover
+          poster="/images/service4.png" @click="$event.target.webkitRequestFullscreen()"
+        >
           <source src="/images/test.mp4" type="video/mp4">
         </video>
       </div>
